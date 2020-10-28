4c8525ce2486ee3684c6e9b849dd13fcc4a29f64
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2020-10-28 07:57:55 +0700

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+    <HeaderShayna />
+    <!-- Header End -->
+
+    <!-- Hero Section Begin -->
+    <HeroShayna />
+    <!-- Hero Section End -->
+
+    <!-- Women Banner Section Begin -->
+    <WomenShayna />
+    <!-- Women Banner Section End -->
+
+    <!-- Instagram Section Begin -->
+    <InstaShayna />
+    <!-- Instagram Section End -->
+
+    <!-- Partner Logo Section Begin -->
+    <PartnerShayna />
+    <!-- Partner Logo Section End -->
+
+    <!-- Footer Section Begin -->
+    <FooterShayna />
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
+</script>
